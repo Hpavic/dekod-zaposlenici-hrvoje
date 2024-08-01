2d773d0ed4a464d8a629da870ec708d88cc9f286
Hpavic <hrvoje.pavic994@gmail.com>
2024-08-01 18:57:53 +0200

    Fetch api and display

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { EmployeeService } from '../services/employee.service';
+import { CommonModule, DatePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Observer } from 'rxjs';
+import { Employee, ApiResponse } from '../models/employee.model';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DatePipe, HttpClientModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[] = [];
+  errorMessage: string | null = null;
+  isMobile: boolean = false;
 
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    this.checkScreenSize();
+    this.fetchEmployees();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize(): void {
+    this.isMobile = window.innerWidth <= 768;
+  }
+
+  fetchEmployees(): void {
+    const observer: Observer<ApiResponse> = {
+      next: data => {
+        if (data.success) {
+          this.employees = data.data;
+          this.errorMessage = null;
+        }
+      },
+      error: error => {
+        this.errorMessage = error.message;
+      },
+      complete: () => {
+        console.log('Fetch employees complete');
+      }
+    };
+
+    this.employeeService.getEmployees().subscribe(observer);
+  }
+
+  retry(): void {
+    this.employeeService.clearCache();
+    this.fetchEmployees();
+  }
 }
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError, retry, of, tap } from 'rxjs';
+import { Employee, ApiResponse } from '../models/employee.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private apiUrl = 'https://api.test.ulaznice.hr/paganini/api/job-interview/employees';
+  private employeesCache: Employee[] | null = null;
+
+  constructor(private http: HttpClient) { }
+
+  getEmployees(): Observable<ApiResponse> {
+    if (this.employeesCache) {
+        console.log('Fetching employees from cache');
+        // If data is cached, return it as an observable
+        return of({ success: true, data: this.employeesCache });
+    }
+
+    console.log('Fetching employees from API');
+    // If data is not cached, make an HTTP request
+    return this.http.get<ApiResponse>(this.apiUrl).pipe(
+        tap(data => {
+            if (data.success) {
+                this.employeesCache = data.data;
+            }
+        }),
+        retry(3),
+        catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMsg = 'An unknown error occurred!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side or network error
+      errorMsg = `An error occurred: ${error.error.message}`;
+    } else {
+      // Backend returned an unsuccessful response code
+      errorMsg = `Backend returned code ${error.status}, body was: ${error.message}`;
+    }
+
+    console.error(errorMsg);
+    return throwError(() => new Error(errorMsg));
+  }
+
+  clearCache() {
+    this.employeesCache = null;
+  }
+}
